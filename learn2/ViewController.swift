8ad0077798e27e68e2ef661927143ba31d8eda29
tiendvyopaz <tiendv@yopaz.vn>
2021-08-11 11:16:24 +0700

    Init

---
+++ learn2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  learn2
+//
+//  Created by Tien Doan on 09/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	var i = 1
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
